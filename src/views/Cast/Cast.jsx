83e0543cc088c8824af5a9f8698dc0aa5c4befe2
ViktoriiaPavlenko <vktr9999@ukr.net>
2022-01-11 20:48:41 +0200

    added no cast message

--- src/views/Cast/Cast.jsx
+++ src/views/Cast/Cast.jsx
@@ -4,7 +4,7 @@ import * as apiService from '../../services/apiService';
 import styles from './Cast.module.css';
 
 export default function Cast({ movieId }) {
-  const [credits, setCredits] = useState([]);
+  const [credits, setCredits] = useState(null);
 
   useEffect(() => {
     apiService
@@ -13,26 +13,34 @@ export default function Cast({ movieId }) {
   }, [movieId]);
 
   return (
-    <ul id="cast" className={styles.list}>
-      {credits &&
-        credits.map(credit => (
-          <li key={credit.id} className={styles.item}>
-            <img
-              src={
-                credit.profile_path
-                  ? `https://image.tmdb.org/t/p/w300${credit.profile_path}`
-                  : 'https://pomogaetsrazu.ru/images/offers/2829219234.jpg'
-              }
-              alt={credit.original_name}
-              className={styles.image}
-            />
-            <div>
-              <h3>{credit.original_name}</h3>
-              <p>Character: {credit.character}</p>
-            </div>
-          </li>
-        ))}
-    </ul>
+    <div>
+      {credits && credits.length === 0 && (
+        <p className={styles.noCast}>
+          We don't have a list of cast for this movie.
+        </p>
+      )}
+      {credits && (
+        <ul id="cast" className={styles.list}>
+          {credits.map(credit => (
+            <li key={credit.id} className={styles.item}>
+              <img
+                src={
+                  credit.profile_path
+                    ? `https://image.tmdb.org/t/p/w300${credit.profile_path}`
+                    : 'https://pomogaetsrazu.ru/images/offers/2829219234.jpg'
+                }
+                alt={credit.original_name}
+                className={styles.image}
+              />
+              <div>
+                <h3>{credit.original_name}</h3>
+                <p>Character: {credit.character}</p>
+              </div>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
   );
 }
 
